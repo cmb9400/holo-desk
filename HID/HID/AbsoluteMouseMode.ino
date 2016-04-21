2dc2ee3e68541f9813ac7963f7e8eb46553e83b0
Carter Buce <cmb9400@rit.edu>
2016-04-21 18:29:39 -0400

    Updated to work with Raspberry Pi.

--- HID/HID/AbsoluteMouseMode.ino
+++ HID/HID/AbsoluteMouseMode.ino
@@ -1,32 +1,50 @@
-/*
-  Copyright (c) 2014-2015 NicoHood
-  See the readme for credit to other people.
-  AbsoluteMouse example
-  Press a button to click, moveTo the mouse.
-  You may also use SingleAbsoluteMouse to use a single report.
-  See HID Project documentation for more infos
-  https://github.com/NicoHood/HID/wiki/Mouse-API
-  https://github.com/NicoHood/HID/wiki/AbsoluteMouse-API
-*/
-
 #include "HID-Project.h"
+//  See HID Project documentation for more infos
+//  https://github.com/NicoHood/HID/wiki/Mouse-API
+//  https://github.com/NicoHood/HID/wiki/AbsoluteMouse-API
 
 void setup() {
-
-  // Sends a clean report to the host. This is important on any Arduino type.
   AbsoluteMouse.begin();
+
+  Serial.begin(9600);    // serial / USB port
+  Serial1.begin(9600);   // Arduino RX - white, Arduino TX - green
+  
+  delay(2000);
+  Serial.print("starting");
 }
 
+
+
 void loop() {
+  String cmd = "";
+  //command terminates with '.'
+
+  // check if byte available on the software serial port
+  if(Serial1.available()){
+    cmd = (Serial1.readStringUntil('.'));
+
+    if (cmd.charAt(0) == 'c') { //click
+      AbsoluteMouse.click();
+      Serial.println("click");
+    } 
     // Move to coordinate (16bit signed, -32768 - 32767)
-    // Moving to the same position twice will not work!
-    // X and Y start in the upper left corner.
-    AbsoluteMouse.moveTo(0, 0);
-    delay(1000);
-    AbsoluteMouse.moveTo(1024,1024);
-    delay(1000);
-    AbsoluteMouse.moveTo(5000,5000);
-    delay(1000);
-
-
+    else if (cmd.charAt(0) == 'm') { //move
+      String dx = cmd.substring(1,7);
+      String dy = cmd.substring(8,13);
+      float x = dx.toFloat();
+      float y = dy.toFloat();
+      AbsoluteMouse.moveTo(x,y);
+      Serial.println("drag " + dx + "," + dy);
+    } 
+    else if (cmd.charAt(0) == 'd') { //drag
+      AbsoluteMouse.press();
+    }
+    else if (cmd.charAt(0) == 'r') { //release
+      AbsoluteMouse.release();
+    }
+    else {
+      Serial.println("invalid");
+      Serial.println(cmd);
+    }
+  }
 }
