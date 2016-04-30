d6939e0da57e353262c00bef85a5a7048c00f569
Dylan Vuz <vuzdylan@gmail.com>
2016-04-30 18:06:54 -0400

    swag drag and droop

--- mouse/mouse.ino
+++ mouse/mouse.ino
@@ -1,22 +1,50 @@
 #include <Mouse.h>
 
-boolean click = false;
+enum states {
+  NO_CLICK,
+  CLICK,
+  HOLD,
+  DRAG,
+  RELEASE,
+};
+
+enum states state = NO_CLICK;
 
 void setup() {
   Serial.begin(9600);
+  Mouse.begin();
   attachInterrupt(digitalPinToInterrupt(2), interrupt, RISING);
 }
 
 void loop() {
-  if (click) {
-    Mouse.click();
-    click = false;
+  switch(state) {
+    case CLICK:
+      Mouse.click();
+      state = NO_CLICK;
+      break;
+    case HOLD:
+      Mouse.press();
+      state = DRAG;
+      break;
+    case RELEASE:
+      Mouse.release();
+      state = NO_CLICK;
+      break;
   }
 }
 
 void interrupt() {
   detachInterrupt(digitalPinToInterrupt(2));
-  click = true;
+  switch(state) {
+    case NO_CLICK:
+      state = CLICK;
+      break;
+    case CLICK:
+      state = HOLD;
+      break;
+    case DRAG:
+      state = RELEASE;
+  }
   attachInterrupt(digitalPinToInterrupt(2), interrupt, RISING);
 }
 
