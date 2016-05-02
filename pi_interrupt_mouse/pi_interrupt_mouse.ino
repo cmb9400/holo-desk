5347d9700a8cc0e722f0d66165cb4dde0f09b62e
Wade Mauger <wadeanthony0100@gmail.com>
2016-05-02 15:27:15 -0400

    Add Alt to allow multiple serials to Leonardo

--- pi_interrupt_mouse/pi_interrupt_mouse.ino
+++ pi_interrupt_mouse/pi_interrupt_mouse.ino
@@ -1,24 +1,53 @@
 #include "HID-Project.h"
+#include <SoftwareSerial.h> //a dependancy to allow us to differentiate input from the pi and from the mega
+#include <AltSoftSerial.h>
+
 //  See HID Project documentation for more infos
 //  https://github.com/NicoHood/HID/wiki/Mouse-API
 //  https://github.com/NicoHood/HID/wiki/AbsoluteMouse-API
 
+// See AltSoftSerial for more info
+// http://www.pjrc.com/teensy/td_libs_AltSoftSerial.html
+// https://github.com/PaulStoffregen/AltSoftSerial
 
-enum states { // fuck you wade
-  NO_CLICK, // No tap has been detected yet
-  CLICK, // A single tap has been detected
-  HOLD, // A double tap has been detected
-  DRAG, // No tap has been detected since Hold was sent
-  RELEASE, // A tap has been detected during the Drag state
+AltSoftSerial mySerial;
+
+enum states {
+  NO_CLICK,
+  CLICK,
+  HOLD,
+  DRAG,
+  RELEASE,
 };
 
-enum states state = NO_CLICK; // Set the init state too no click
+enum states state = NO_CLICK;
+
+//SoftwareSerial mySerial(10 , 11);
+
+void interrupt() {
+  //AbsoluteMouse.moveTo(32767,32767);
+  //detachInterrupt(digitalPinToInterrupt(2));
+  switch(state) {
+    case NO_CLICK:
+      state = CLICK;
+      break;
+    case CLICK:
+      state = HOLD;
+      break;
+    case DRAG:
+      state = RELEASE;
+  }
+  //attachInterrupt(digitalPinToInterrupt(2), interrupt, RISING);
+}
 
 void setup() {
-  Serial.begin(9600);    // serial / USB port
-  Serial1.begin(9600);   // Arduino RX - white, Arduino TX - green
+  Serial.begin(115200);    // serial / USB port
+  //while (!Serial) {}
+  //altSerial.begin(115200);
+  mySerial.begin(115200);
+  //while (!mySerial) {}
   AbsoluteMouse.begin();
-  attachInterrupt(digitalPinToInterrupt(2), interrupt, RISING); //interrupt pin from Mega (hock up pin 35 mega to 2 leo)
+  attachInterrupt(digitalPinToInterrupt(2), interrupt, RISING); //interrupt pin from Mega
   
   delay(2000);
 }
@@ -48,29 +77,17 @@ void loop() {
 
   // check if byte available on the software serial port
   //receives zero-padded string of 16 bit coordinates (xy) ending with a '.'
-  if(Serial1.available()){
-    cmd = (Serial1.readStringUntil('.'));
-    //Serial.println(cmd);
-    String dx = cmd.substring(0,4);
+  if (mySerial.available()) {
+    cmd = (mySerial.readStringUntil('.'));
+    mySerial.println(cmd);
+    String dx = cmd.substring(0,5);
     String dy = cmd.substring(5,9);
-    float x = dx.toFloat();
-    float y = dy.toFloat();
+    int x = dx.toInt();
+    int y = dy.toInt();
     AbsoluteMouse.moveTo(x,y);
-  } 
-  
-}
-
-void interrupt() {
-  //detachInterrupt(digitalPinToInterrupt(2));
-  switch(state) {
-    case NO_CLICK:
-      state = CLICK;
-      break;
-    case CLICK:
-      state = HOLD;
-      break;
-    case DRAG:
-      state = RELEASE;
   }
-  //attachInterrupt(digitalPinToInterrupt(2), interrupt, RISING);
+  else {
+    Serial.println("Serial connection failed");
+  }
+  
 }
