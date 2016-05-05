65ea3d35cea04013fc84ee6bdf05e37f9234fe93
holodesk <chh2538@rit.edu>
2016-05-05 17:59:12 -0400

    add auto re-connect

--- MouseStreamNet.cs
+++ MouseStreamNet.cs
@@ -14,25 +14,27 @@ namespace Microsoft.Samples.Kinect.DepthBasics
         private TcpClient client = null;
         private StreamWriter netStream = null;
         private bool enabled;
+        private DateTime lastConnectAttempted;
+        private string address;
+        private int port;
 
         public MouseStreamNet(bool enable = true, string address = "129.21.207.17", int port = 1313)
         {
             this.enabled = enable;
             if (this.enabled)
             {
+                this.address = address;
+                this.port = port;
                 try
                 {
-                    this.client = new TcpClient(address, port);
+                    connect();
                 }
                 catch (SocketException se)
                 {
                     this.enabled = false;
                     Console.WriteLine("failed to connect to mouse");
                     Console.WriteLine(se.Message);
-                    return;
                 }
-                NetworkStream ns = this.client.GetStream();
-                this.netStream = new StreamWriter(ns);
             }
         }
 
@@ -47,7 +49,30 @@ namespace Microsoft.Samples.Kinect.DepthBasics
             } catch (Exception e)
             {
                 Console.Error.WriteLine(e.Message);
+                if ((DateTime.Now - lastConnectAttempted).Seconds > 3)
+                {
+                    try
+                    {
+                        Console.WriteLine("Attempting to reconnect");
+                        connect();
+                    } catch (SocketException)
+                    {
+                        Console.WriteLine("Reconnect failed");
+                    }
+                }
             }
+        }
+
+
+        private void connect()
+        {
+            if (!this.enabled) return;
+            this.lastConnectAttempted = DateTime.Now;
+            this.client = new TcpClient();
+            client.ReceiveTimeout = 100;
+            this.client.Connect(address, port);
+            NetworkStream ns = this.client.GetStream();
+            this.netStream = new StreamWriter(ns);
         }
 
     }
